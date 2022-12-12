a9ef45400d816b15e70a4bbd0b661b3d842260e8
Sawan Somaiah <103037990+sawan-somaiah1349@users.noreply.github.com>
2022-12-12 22:04:56 +0530

    New

---
+++ Calculator/script.js
@@ -0,0 +1,69 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+const display_output =  document.querySelector('.display .output');
+
+let input = "";
+
+for(let key of keys)
+{
+    const value = key.dataset.key;
+
+    key.addEventListener('click', ()=>{
+
+    if(value == "clear"){
+    input = "";
+    display_input.innerHTML = '';
+    display_output.innerHTML = '';
+    }
+    else if(value == "backspace"){
+        input = input.slice(0,-1)
+        display_input.innerHTML = cleanInput(input);
+
+    }
+    else if(value == "=")
+    {
+        let result = eval(input);
+        display_output.innerHTML = result;
+    }
+    else if (value == "brackets"){
+        if(input.indexOf("(") == -1 || 
+           input.indexOf("(") != -1 && 
+           input.indexOf(")") != -1 && 
+           input.lastIndexOf("(") < input.lastIndexOf(")"))
+            {
+                input += "(";
+            }
+        else if(input.indexOf("(") != -1 &&
+                input.indexOf(")") == -1 || 
+                input.indexOf(")") != -1 && 
+                input.indexOf("(") != -1 && 
+                input.lastIndexOf("(") > input.lastIndexOf(")"))
+                {
+                    input += ")";
+                }
+        display_input.innerHTML = cleanInput(input);
+        }
+    else{
+    input += value;
+    display_input.innerHTML = cleanInput(input);
+    }
+    })
+}
+
+function cleanInput(input){
+    let inputArray = input.split("");
+    let inputArrayLength = inputArray.length;
+
+    for( let i = 0 ; i<=inputArrayLength;i++)
+    {
+        if(inputArray[i] == "*"){
+            inputArray[i] = '<span class="operator">x</span>';
+        }
+        else if(inputArray == "/")
+        {
+            inputArray[i] = '<span class="operator">÷</span>'
+        }
+    }
+    return inputArray.join("");
+}
+
